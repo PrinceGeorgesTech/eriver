1ff911c0390d1e7ecb0b6efb9773933aea95a08f
Fareed Javed <fareedjaved@Fareeds-MBP.home>
2015-01-23 00:44:19 -0500

    We created a template

--- eriver.py
+++ eriver.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('base.html', 
+    	title = "Home", 
+    	header = "Information", 
+    	content = "Content is here")
 
 if __name__ == "__main__":
     app.run()
