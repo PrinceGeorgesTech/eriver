b3b9cba7d5af038e5c9165145dad2e09e5c4fd37
ramirezg <gabriel.e.ramirez@gmail.com>
2015-01-25 22:44:53 -0500

    added bootstrap to app

--- eriver.py
+++ eriver.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 @app.route("/")
 def index():
-    return render_template('base.html', 
-    	title = "Home", 
-    	header = "Information", 
-    	content = "Content is here")
+    return render_template(
+        'base.html',
+        title="Home",
+        header="Information",
+        content="Content is here")
 
 if __name__ == "__main__":
     app.run()
