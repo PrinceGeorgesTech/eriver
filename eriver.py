dc7bc7fcf42129ab9963f17d61b65a408acfc134
Sana Javed <sanajaved7@gmail.com>
2015-01-25 23:29:13 -0500

    Added Google Calendar to Events page and fixed nav bar

--- eriver.py
+++ eriver.py
@@ -12,13 +12,33 @@ def index():
 
 @app.route("/events")
 def events():
-    return render_template('base.html')
+    return render_template('events.html')
 
 
 @app.route("/info")
 def info():
     return render_template('base.html')
 
+@app.route("/libraries")
+def libraries():
+    return render_template('base.html',
+        title = "Libraries in East Riverdale",
+        header = "Libraries",
+        content = "Visit a local library below!")
+
+@app.route("/schools")
+def schools():
+    return render_template('base.html',
+        title = "Public Schools in East Riverdale",
+        header = "Public Schools",
+        content = "Need info on a public school? See below!")
+
+@app.route("/officials")
+def officials():
+    return render_template('base.html',
+        title = "Elected Officials for the East Riverdale Community",
+        header = "Elected Officials for East Riverdale",
+        content = "Need to contact an elected official? See below!")
 
 if __name__ == "__main__":
     app.run(debug=True)
